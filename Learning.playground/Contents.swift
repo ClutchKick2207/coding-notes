f39a94223cb775118b0a9d5f23f6a5044b2ea02d
Ariz Ahmed <ariz.ahmed@wellard.net.au>
2020-11-29 19:20:15 +0800

    Swift Learning (added)

---
+++ Learning.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+var str = "Hello, world!"
+
+print(str)
+
+//Variables (can be reassigned)
+var a = 2
+var b = 1
+
+a = 1 //no need to declare again
+
+//Operations
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+//Constants (CANNOT be reassigned)
+let c = 10
+
+/* Data Types:
+String: Text
+Integer: Whole Numbers (both positive and negative)
+Float: Decimal Numbers
+Double: Large Decimal Numbers
+Boolean: True/False
+*/
