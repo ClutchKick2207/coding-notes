195bedf2d8a769487ccdc96de6467bc8fb1e256c
Ariz Ahmed <ariz.ahmed@wellard.net.au>
2020-11-29 20:14:41 +0800

    Edits

--- Learning.playground/Contents.swift
+++ Learning.playground/Contents.swift
@@ -4,9 +4,13 @@ var str = "Hello, world!"
 
 print(str)
 
+//Format to declare Variables/Constants
+
+var name:String = "How to properly assign a data-type"
+
 //Variables (can be reassigned)
-var a = 2
-var b = 1
+var a:Int = 2
+var b:Int = 1
 
 a = 1 //no need to declare again
 
